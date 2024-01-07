10b9f5dc7e0c4836d14e446afdfad23b4c4a7682
AhmadOmar <ahmadomarns@gmail.com>
2024-01-07 23:57:45 +0200

    Update models with new properties

--- src/models/room.ts
+++ src/models/room.ts
@@ -1,13 +1,14 @@
 import { Amenity } from "./amenity"
 
 export  interface Room {
-    roomNumber: string,
-    roomType: string,
-    price: number,
+    id?: number,
+    roomNumber?: string,
+    roomType?: string,
+    price?: number,
     roomAmenities?: Amenity[],
-    capacityOfAdults: number,
-    capacityOfChildren: number,
-    roomPhotoUrl: string,
+    capacityOfAdults?: number,
+    capacityOfChildren?: number,
+    roomPhotoUrl?: string,
     description?: string
     availability?: boolean,
     hotelId?: number
